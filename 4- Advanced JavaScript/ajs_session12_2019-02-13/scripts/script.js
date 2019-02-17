c70fa5ec2212b7d6e19318fac20df6909898e3bb
SameerAli2808 <SameerAli2808@users.noreply.github.com>
2019-02-17 14:11:23 +0100

    adding options of creating ship

--- 4- Advanced JavaScript/ajs_session12_2019-02-13/scripts/script.js
+++ 4- Advanced JavaScript/ajs_session12_2019-02-13/scripts/script.js
@@ -118,6 +118,12 @@ $(() => {
         if(ships === 'fighter') {
             populateFighter()
         }
+        if(ships === 'cargo') {
+            populateCargo()
+        }
+        if(ships === 'mining') {
+            populateMining()
+        }
     })
 })
 
