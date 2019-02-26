b1c63727cbbcc3c2dba684521a0473e709c8b115
SameerAli2808 <SameerAli2808@users.noreply.github.com>
2019-02-26 19:27:51 +0100

    First

---
+++ 05- Programming with C# and Visual Studio 2015 – Basic/csb_session01_2019-02-25/csb_s01/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("    *    ");
+            Console.WriteLine("   ***   ");
+            Console.WriteLine(" ******* ");
+            Console.WriteLine("*********");
+            Console.WriteLine("");
+            Console.WriteLine("##########");
+            Console.WriteLine("Exercise 4");
+            Console.WriteLine("##########");
+            Console.WriteLine("");
+            Console.WriteLine("   *  ");
+            Console.WriteLine("   *  ");
+            Console.WriteLine("  *** ");
+            Console.WriteLine(" * * *");
+            Console.WriteLine("   *  ");
+            Console.WriteLine("   *  ");
+            Console.WriteLine(" *  * ");
+            Console.WriteLine("*    *");
+            //Thread.Sleep(5000);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 05- Programming with C# and Visual Studio 2015 – Basic/csb_session01_2019-02-25/csb_s01_hm/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Stop");
+            Thread.Sleep(5000);
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Prepare");
+            Thread.Sleep(5000);
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Go");
+            Thread.Sleep(5000);
+            Console.ReadLine();
+        }
+    }
+}
