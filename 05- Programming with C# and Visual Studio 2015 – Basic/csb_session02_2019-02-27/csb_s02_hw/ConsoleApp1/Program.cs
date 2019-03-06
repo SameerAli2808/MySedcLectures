7e3de50464aa609dc915ecfc8d76ec3609959a28
SameerAli2808 <SameerAli2808@users.noreply.github.com>
2019-03-06 15:37:11 +0100

    new

--- 05- Programming with C# and Visual Studio 2015 – Basic/csb_session02_2019-02-27/csb_s02_hw/ConsoleApp1/Program.cs
+++ 05- Programming with C# and Visual Studio 2015 – Basic/csb_session02_2019-02-27/csb_s02_hw/ConsoleApp1/Program.cs
@@ -10,6 +10,46 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            double result = 0;
+            double firstNo;
+            double secondNo;
+            string operand;
+            bool firstNoBool;
+            bool secondNoBool;
+
+                Console.WriteLine("Enter first number!");
+                string firstNoString = Console.ReadLine();
+                firstNoBool = double.TryParse(firstNoString, out firstNo);
+                
+                Console.WriteLine("Enter the operand (+, -, * or /)");
+                operand = Console.ReadLine();
+                
+                Console.WriteLine("Enter second number!");
+                string secondNoString = Console.ReadLine();
+                secondNoBool = double.TryParse(secondNoString, out secondNo);
+                
+            switch (operand)
+            {
+                case "+":
+                    result = firstNo + secondNo;
+                    break;
+                case "-":
+                    result = firstNo - secondNo;
+                    break;
+                case "*":
+                    result = firstNo * secondNo;
+                    break;
+                case "/":
+                    result = firstNo / secondNo;
+                    break;
+                default:
+                    Console.WriteLine("Enter proper operand, please!");
+                    break;
+            }
+
+            Console.WriteLine("The result of ( " + firstNo + " " + operand + " " + secondNo + " ) is " + result);
+
+            Console.ReadLine();
         }
     }
 }
---
+++ 05- Programming with C# and Visual Studio 2015 – Basic/csb_session02_2019-02-27/csb_s02_hw_p2/csb_s02_hw_p2/ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double denToEur = 62.2;
+            double denToUsd = 53.1;
+            double denToGbp = 71.6;
+            double exchanedAmount = 0;
+
+            Console.WriteLine("Enter the amount in deners!");
+            string amountString = Console.ReadLine();
+            double amount;
+            bool isAmountBool = double.TryParse(amountString, out amount);
+
+            Console.WriteLine("Enter the currancy you want to exchange to (EUR, USD or GBP)");
+            string currancy = Console.ReadLine();
+
+            switch (currancy)
+            {
+                case "EUR":
+                    exchanedAmount = amount / denToEur;
+                    break;
+                case "USD":
+                    exchanedAmount = amount / denToUsd;
+                    break;
+                case "GBP":
+                    exchanedAmount = amount / denToGbp;
+                    break;
+                default:
+                    Console.WriteLine("No such currancy, sorry!");
+                    break;
+            }
+
+            Console.WriteLine(String.Format("{0:000,0.00}", amount) + " deners are " + String.Format("{0:000,0.00}", exchanedAmount) + " " + currancy );
+            Console.ReadLine();
+        }
+    }
+}
