29e490525dcd3ceddce4e9f092f8faf24b876d80
SameerAli2808 <SameerAli2808@users.noreply.github.com>
2019-03-06 14:52:09 +0100

    added codes

---
+++ 05- Programming with C# and Visual Studio 2015 – Basic/csb_session03_2019-03-04/csb_s03_hm/csb_s03_hm/ConsoleApp1/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double result = 0;
+            double firstNo;
+            double secondNo;
+            string operand;
+            bool firstNoBool;
+            bool secondNoBool;
+
+            do
+            {
+                Console.WriteLine("Enter first number!");
+                string firstNoString = Console.ReadLine();
+                firstNoBool = double.TryParse(firstNoString, out firstNo);
+                if (!(firstNoBool))
+                {
+                    Console.WriteLine("Cannot enter any charetor rather than number!");
+                }
+            } while (!(firstNoBool));
+
+            do
+            {
+                Console.WriteLine("Enter the operand (+, -, * or /)");
+                operand = Console.ReadLine();
+                if((operand != "+") && (operand != "-") && (operand != "*") && (operand != "/"))
+                {
+                    Console.WriteLine("Enter oprand only!");
+                }
+            } while ((operand != "+") && (operand != "-") && (operand != "*") && (operand != "/"));
+            
+
+            do
+            {
+                Console.WriteLine("Enter second number!");
+                string secondNoString = Console.ReadLine();
+                secondNoBool = double.TryParse(secondNoString, out secondNo);
+                if (!(secondNoBool))
+                {
+                    Console.WriteLine("Cannot enter any charetor rather than number!");
+                }
+            } while (!(secondNoBool));
+
+            switch (operand)
+            {
+                case "+":
+                    result = firstNo + secondNo;
+                    break;
+                case "-":
+                    result = firstNo - secondNo;
+                    break;
+                case "*":
+                    result = firstNo * secondNo;
+                    break;
+                case "/":
+                    result = firstNo / secondNo;
+                    break;
+                default:
+                    Console.WriteLine("Enter proper operand, please!");
+                    break;
+            }
+
+            Console.WriteLine("The result of ( " + firstNo + " " + operand + " " + secondNo + " ) is " + result);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 05- Programming with C# and Visual Studio 2015 – Basic/csb_session03_2019-03-04/csb_s03_hw_p2/studentGroup/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studentGroup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] studentsG1 = new string[5] { "Sameer", "Damjan", "David", "Stefani", "Bojana" };
+            string[] studentsG2 = new string[5] { "Borslav", "Martin", "Dejan", "Flora", "Igor" };
+            string[] studentsG3 = new string[5] { "Reste", "Dragan", "Sinisha", "Slavana", "Dimitar" };
+            string[] studentChosen = new string[5];
+
+            Console.WriteLine("Choose group from 1 to 3, please!");
+            string groupString = Console.ReadLine();
+            int group;
+            bool groupBool = int.TryParse(groupString, out group);
+
+            switch (group)
+            {
+                case 1:
+                    studentChosen = studentsG1;
+                    Console.WriteLine("The student in group G1 are:");
+                    break;
+                case 2:
+                    studentChosen = studentsG2;
+                    Console.WriteLine("The student in group G2 are:");
+                    break;
+                case 3:
+                    studentChosen = studentsG3;
+                    Console.WriteLine("The student in group G3 are:");
+                    break;
+                default:
+                    Console.WriteLine("There's no such a group");
+                    break;
+            }
+
+            for (int i = 0; i < studentChosen.Length; i++)
+            {
+                Console.WriteLine(studentChosen[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
