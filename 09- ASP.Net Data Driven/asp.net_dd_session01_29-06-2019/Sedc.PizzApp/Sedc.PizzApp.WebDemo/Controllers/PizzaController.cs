17fc127e4bfd3e4adaf10bb888a894f5a5ed390f
SameerAli2808 <SameerAli2808@users.noreply.github.com>
2019-07-08 00:39:15 +0200

    homework

---
+++ 09- ASP.Net Data Driven/asp.net_dd_session01_29-06-2019/Sedc.PizzApp/Sedc.PizzApp.WebDemo/Controllers/PizzaController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PizzApp.Models;
+using PizzApp.Repositories.Abstractions;
+using PizzApp.Repositories.EntityFramework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sedc.PizzApp.WebDemo.Controllers
+{
+    public class PizzaController : Controller
+    {
+        private readonly IPizzaRepository pizzaRepository;
+
+        public PizzaController()
+        {
+            //pizzaRepository = new InMemoryPizzaRepository();
+            pizzaRepository = new EntityFrameworkPizzaRepository();
+        }
+
+        public IActionResult Details(int id)
+        {
+            var pizza = pizzaRepository.GetById(id);
+            return View(pizza);
+        }
+
+        public IActionResult Index()
+        {
+            //TIPP: how foreach works
+            //IEnumerator<string> enumerator = pizzas.GetEnumerator();
+            //enumerator.Reset();
+            //while (enumerator.MoveNext())
+            //{
+            //    //do something with the current value;
+            //    //enumerator.Current;
+            //}
+
+            //ViewData["pizzas"] = pizzas;
+
+            var pizzas = pizzaRepository.GetAllPizzas();
+            return View(pizzas);
+        }
+
+        //get pizza that is longer than 4 character
+        public IActionResult GetPizzaLongerThan4()
+        {
+            ViewBag.Pizzas = new List<string>
+            {
+                "capri", "tuna","margarita","pepperoni"
+            }.Where(p => p.Length > 4);
+            return View("~/Views/Pizza/GetAll.cshtml");
+        }
+        public IActionResult TestView()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            //get the view for creating pizza
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Pizza model)
+        {
+            //submit the pizza from form
+            var newPizza = pizzaRepository.Create(model);
+
+            return RedirectToAction("Details", new
+            {
+                id = newPizza.Id,
+            });
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var pizza = pizzaRepository.GetById(id);
+            return View(pizza);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int Id, Pizza model)
+        {
+            var pizza = pizzaRepository.Update(model);
+            return RedirectToAction("Details", new
+            {
+                id = pizza.Id
+            });
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var pizza = pizzaRepository.GetById(id);
+            return View(pizza);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int Id, Pizza model)
+        {
+            pizzaRepository.Delete(Id);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ 09- ASP.Net Data Driven/asp.net_dd_session02_01-07-2019/Sedc.PizzApp/ado.net/AdoNetDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Data.SqlClient;
+
+namespace ado.net
+{
+    class Program
+    {
+        private const string connStr = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=PizzaDatabase01;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        static void Main(string[] args)
+        {
+            var connection = new SqlConnection("");
+            connection.Open();
+            //////////////
+
+            string query = "select count(*) from Pizzas";
+            var command = new SqlCommand(query, connection);
+            var numberOfPizzas  = command.ExecuteScalar();
+            Console.WriteLine(numberOfPizzas);
+
+
+            /////////////
+            connection.Close();
+        }
+    }
+}
